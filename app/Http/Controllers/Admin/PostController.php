a9cbf5ac28ebecd3c16e0c32def61af75a68dcba
Fabio Moro <fabio.moro2000@gmail.com>
2022-07-19 15:19:36 +0200

    aggiunta creazione e lettura

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -14,7 +16,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -24,7 +27,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -35,7 +38,14 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $newPost = new Post();
+        $newPost->fill($data);
+        $newPost->slug = $this->getSlug($data['title']);
+        $newPost->published = isset($data['published']); 
+        $newPost->save();
+   
+        return redirect()->route('admin.posts.index');
     }
 
     /**
@@ -81,5 +91,18 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function getSlug($title)
+    {
+        $slug = Str::of($title)->slug('-');
+        $count = 1;
+
+        while( Post::where('slug', $slug)->first() ) {
+            $slug = Str::of($title)->slug('-') . "-{$count}";
+            $count++;
+        }
+
+        return $slug;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,14 +21,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::middleware('auth')
-    ->namespace('Admin')
-    ->name('admin.')
-    ->prefix('admin')
-    ->group(function() {
-        Route::get('/home', 'HomeController@index')->name('home');
-    });
+   ->namespace('Admin')
+   ->name('admin.')
+   ->prefix('admin')
+   ->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
 
-Route::any('{any?}', function() {
-    return view('guest.home');
-})->where('any','.*');
-
+Route::get("{any?}", function() {
+    return view("guest.home");
+})->where("any", ".*");
